a352bcb7a8db94fcd38b156b890c3a2c06c3686c
JPC <jcf@perucodex.com>
2025-06-30 15:35:19 -0500

    Modulo odoo c

---
+++ jp_crack_odoo/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Crack Odoo",
+
+    'summary': """
+        Crack Odoo Enterprise""",
+
+    'description': """
+        Crack Odoo Enterprise Edition with a 30 year license
+    """,
+
+    'author': "__Peruvian_Chicken__",
+    'website': "http://www.elinge.com",
+    'version': '0.1',
+    'depends': ['base','mail'],
+    'license': 'OEEL-1',
+
+
+}
